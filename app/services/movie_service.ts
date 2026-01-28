d85875663959d2705e5dac34991d2c4cae038a71
T-ASWIN <aswinmani53@gmail.com>
2026-01-28 10:24:58 +0530

    27/1/25 all files upload

--- app/services/movie_service.ts
+++ app/services/movie_service.ts
@@ -1,6 +1,40 @@
-import app from '@adonisjs/core/services/app'
-import { readFile, readdir } from 'node:fs/promises'
-import { MarkdownFile } from '@dimerapp/markdown'
-import { Exception } from '@adonisjs/core/exceptions'
+import Movie from "#models/movie";
 
-export default class MovieService {}
+type movieSortOptions={
+    id:string 
+    text:string
+    field:string
+    dir:'asc'|'desc'|undefined
+}
+export default class MovieService {
+
+
+        static sortOptions:movieSortOptions[]=[
+            {id:'title_asc',text:'Title (asc)',field:'title',dir:'asc'},
+            {id:'title_desc',text:'Title (desc)',field:'title',dir:'desc'},
+            {id:'releasedAt_asc',text:'Release Date (asc)',field:'releasedAt',dir:'asc'},
+            {id:'releasedAt_desc',text:'Release Date (desc)',field:'releasedAt',dir:'desc'},
+            {id:'writer_asc',text:'Writer Name(asc)',field:'cineasts.last_name',dir:'asc'},
+            {id:'writer_desc',text:'Writer Name(desc)',field:'cineasts.last_name',dir:'desc'},
+        ]
+    
+
+    static getFiltered(filters:Record<string,any>){
+        const sort= this.sortOptions.find((options)=>options.id===filters.sort)||this.sortOptions[0]
+
+        return  Movie.query()
+        .if(filters.search,(query)=>query.whereILike('title',`%${filters.search}%`))
+            .if(filters.status,(query)=>query.where('statusId',filters.status))
+                .if(['writer_asc','writer_desc'].includes(sort.id),(query)=>{
+query.join('cineasts','cineasts.id','writer_id')
+.select('movies.*')
+                })
+                
+                .preload('director')
+        .preload('writer')
+        .preload('status')
+        .orderBy(sort.field, sort.dir)
+        
+        .limit(15)
+    }
+}
